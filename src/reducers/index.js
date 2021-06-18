5199dec779e6cca18128a76086c302cac48b03e5
Chinmaya Jagadev <chinmaya.jagadev@indusnet.co.in>
2021-06-18 23:12:20 +0530

    create the project

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import userDetails from './reducer'
+import {combineReducers} from 'redux';
+const rootReducer = combineReducers({
+    allUserData: userDetails
+})
+
+export default rootReducer
+
+
